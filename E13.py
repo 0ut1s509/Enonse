2d620eb4210546943d1050bde5a1f76e889291d0
Godson JOSEPH <gentlemansondy509@gmail.com>
2022-07-30 17:55:16 -0400

    3

--- E13.py
+++ E13.py
@@ -1,20 +1,3 @@
-sekans=[0,1,2,3,4]
-nsekans=[]
-
-for i,el in enumerate(sekans):
-    if i==0:
-        sekans=sekans[::-1]
-        print(sekans,"\n")
-        
-    if i==1:
-        sekans=list(reversed(sekans[0:-1]))+sekans[-1:]
-        print(sekans,"\n")
-    if i==2:
-        sekans=list(reversed(sekans[0:-2]))+sekans[-2:]
-        print(sekans,"\n")
-    if i==3:
-        sekans=list(reversed(sekans[0:-3]))+sekans[-3:]
-print(sekans[0],"\n")
 
 
 
--- E7.py
+++ E7.py
@@ -1,11 +1,4 @@
-chaine=input("antre chaine karakte a : ")
-chaine=chaine.split()
-voyel="aeiouy"
-nchaine=""
 
-for i,el in enumerate(chaine):
-        print(i)
-print(chaine)
 
         
 
